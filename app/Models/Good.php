5d96caec51dd1054927b15ddaecca187ebfb224c
thegradle <adamchuk.v.1@gmail.com>
2022-12-05 13:15:25 +0200

    Add models and controllers

---
+++ app/Models/Good.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Good extends Model
+{
+    use HasFactory;
+
+    public function characteristics()
+    {
+        return $this->hasMany(Characteristic::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function section()
+    {
+        return $this->hasOne(Section::class);
+    }
+}
