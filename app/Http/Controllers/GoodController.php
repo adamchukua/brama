733f04503dbc46d7f92d6a10ee3b6aed50f5ab8c
thegradle <adamchuk.v.1@gmail.com>
2022-12-06 14:05:14 +0200

    Fix error [Microsoft][ODBC Driver 17 for SQL Server][SQL Server]Conversion failed

--- app/Http/Controllers/GoodController.php
+++ app/Http/Controllers/GoodController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Good;
 use Illuminate\Http\Request;
+use TheSeer\Tokenizer\Exception;
 
 class GoodController extends Controller
 {
-    public function show(Good $good)
+    public function show($id)
     {
+        $good = Good::findOrFail(intval($id));
+
         return view('goods.show', compact('good'));
     }
 }
